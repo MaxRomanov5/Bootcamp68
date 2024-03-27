5f11478d38a0355895fe224b053ee3edc47717a7
Yaroslav Kostak <slavakostak2010@gmail.com>
2024-03-27 13:35:12 +0100

    update employee component

--- src/components/Employee/Employee.jsx
+++ src/components/Employee/Employee.jsx
@@ -1,25 +1,23 @@
-import { useState } from "react"
-
+import { useState } from "react";
 
 const Employee = ({ name, baseSalary }) => {
-    
-    const [salary, setSalary] = useState(baseSalary)
-    const onRaiseClick = () => {
-        return setSalary(salary+100)
-    }
-    const onDownClick = () => {
-        return setSalary(salary-100)
-    }
+  const [salary, setSalary] = useState({ baseSalary, bonus: 50 });
+  const onRaiseClick = () => {
+    return setSalary({ ...salary, baseSalary: salary.baseSalary + 100 });
+  };
+  const onDownClick = () => {
+    return setSalary({ ...salary, baseSalary: salary.baseSalary - 100 });
+  };
   return (
-      <div>
-          <h2>Name: {name}</h2>
-          <p>Base salary: {baseSalary}</p>
-          <p>Total salary: {salary}</p>
+    <div>
+      <h2>Name: {name}</h2>
+      <p>Base salary: {baseSalary}</p>
+      <p>Total salary: {salary.baseSalary + salary.bonus}</p>
 
-          <button onClick={onRaiseClick}>Raise salary</button>
-          <button onClick={onDownClick}>Down salary</button>
+      <button onClick={onRaiseClick}>Raise salary</button>
+      <button onClick={onDownClick}>Down salary</button>
     </div>
-  )
-}
+  );
+};
 
-export default Employee
+export default Employee;
