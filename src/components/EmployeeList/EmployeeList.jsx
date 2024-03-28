f2edd1778d4c2ea6e01e634f91d4f41d89963639
ValentynSkyba <valentynskyba@gmail.com>
2024-03-28 12:38:49 +0000

    ver 1

--- src/components/EmployeeList/EmployeeList.jsx
+++ src/components/EmployeeList/EmployeeList.jsx
@@ -1,35 +1,43 @@
 import { useState } from "react";
 import Employee from "../Employee/Employee";
+import TeamSalary from "../TeamSalary/TeamSalary";
 
 function EmployeeList() {
-    const [bonus, setBonus] = useState(50);
+  const [bonus, setBonus] = useState(50);
+  const [firstTeamSalary, setFirstTeamSalary] = useState(0);
+  const [secondTeamSalary, setSecondTeamSalary] = useState(0);
 
-    function handleRaiseBonus() {
-        setBonus(bonus + 50);
-    }
+  function handleRaiseBonus() {
+    setBonus(bonus + 50);
+  }
 
-    return (
-        <div>
-            <Employee
-                name={"Antonio"}
-                baseSalary={1500}
-                bonus={Number((bonus / 3).toFixed(2))}
-                handleRaiseBonus={handleRaiseBonus}
-            />
-            <Employee
-                name={"Alex"}
-                baseSalary={2000}
-                bonus={Number((bonus / 3).toFixed(2))}
-                handleRaiseBonus={handleRaiseBonus}
-            />
-            <Employee
-                name={"Robin"}
-                baseSalary={4000}
-                bonus={Number((bonus / 3).toFixed(2))}
-                handleRaiseBonus={handleRaiseBonus}
-            />
-        </div>
-    );
+  return (
+    <div>
+      {/* <Employee
+        name={"Antonio"}
+        baseSalary={1500}
+        bonus={Number((bonus / 3).toFixed(2))}
+        handleRaiseBonus={handleRaiseBonus}
+      />
+      <Employee
+        name={"Alex"}
+        baseSalary={2000}
+        bonus={Number((bonus / 3).toFixed(2))}
+        handleRaiseBonus={handleRaiseBonus}
+      />
+      <Employee
+        name={"Robin"}
+        baseSalary={4000}
+        bonus={Number((bonus / 3).toFixed(2))}
+        handleRaiseBonus={handleRaiseBonus}
+      /> */}
+      <TeamSalary
+        name={"firstTeam"}
+        salary={firstTeamSalary}
+        updateSalary={setFirstTeamSalary}
+      />
+    </div>
+  );
 }
 
 export default EmployeeList;
---
+++ src/components/TeamSalary/TeamSalary.jsx
@@ -0,0 +1,11 @@
+function TeamSalary({ name, salary, updateSalary }) {
+  return (
+    <>
+      <p>{name}</p>
+      <p>{salary}</p>
+      <button>Click</button>
+    </>
+  );
+}
+
+export default TeamSalary;
