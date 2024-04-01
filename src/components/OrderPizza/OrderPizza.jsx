673ea461fab9ced129b7ca6df71832be826dd081
kenyafan <kenyafan321@gmail.com>
2024-04-01 14:41:52 +0300

    Update OrderPizza.jsx

--- src/components/OrderPizza/OrderPizza.jsx
+++ src/components/OrderPizza/OrderPizza.jsx
@@ -4,21 +4,61 @@ function OrderPizza() {
   const pizzaNameId = useId();
   const [pizzaName, setPizzaName] = useState("pepperoni");
 
+  const [pizzaSize, setPizzaSize] = useState("l");
+
   const handlePizzaName = (e) => {
     setPizzaName(e.target.value);
   };
 
+  const handlePizzaSize = (e) => {
+    setPizzaSize(e.target.value);
+  };
+
   return (
-    <div>
-      <label htmlFor={pizzaNameId}>
-        Name of pizza
-        <select id={pizzaNameId} value={pizzaName} onChange={handlePizzaName}>
-          <option value="margarita">margarita</option>
-          <option value="pepperoni">pepperoni</option>
-          <option value="4_cheese">4 cheese</option>
-        </select>
-      </label>
-    </div>
+    <>
+      <div>
+        <label htmlFor={pizzaNameId}>
+          Name of pizza
+          <select id={pizzaNameId} value={pizzaName} onChange={handlePizzaName}>
+            <option value="margarita">margarita</option>
+            <option value="pepperoni">pepperoni</option>
+            <option value="4_cheese">4 cheese</option>
+          </select>
+        </label>
+      </div>
+      <div>
+        <label>
+          <input
+            type="radio"
+            name="pizzaSize"
+            value="s"
+            onChange={handlePizzaSize}
+            checked={pizzaSize === "s"}
+          ></input>
+          S
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="pizzaSize"
+            value="m"
+            onChange={handlePizzaSize}
+            checked={pizzaSize === "m"}
+          ></input>
+          M
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="pizzaSize"
+            value="l"
+            onChange={handlePizzaSize}
+            checked={pizzaSize === "l"}
+          ></input>
+          L
+        </label>
+      </div>
+    </>
   );
 }
 
