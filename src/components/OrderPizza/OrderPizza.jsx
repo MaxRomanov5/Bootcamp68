50fbec06c6f4f02422eb97329f984ca47e831a40
Necrotic999 <l.ilya041105@gmail.com>
2024-04-02 14:34:41 +0300

    update

--- src/components/OrderPizza/OrderPizza.jsx
+++ src/components/OrderPizza/OrderPizza.jsx
@@ -1,137 +1,129 @@
 import { useId, useState } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as yup from "yup";
+
+const scheme = yup.object().shape({
+  pizzaName: yup
+    .string()
+    .oneOf(["margarita", "peperoni", "4_cheese"])
+    .required("Required"),
+  pizzaSize: yup.string().oneOf(["s", "m", "l"]).required("Required"),
+});
 
 function OrderPizza() {
-    const pizzaNameId = useId();
-    const [pizzaName, setPizzaName] = useState("pepperoni");
+  const pizzaNameId = useId();
+  const [pizzaName, setPizzaName] = useState("pepperoni");
 
-    const [pizzaSize, setPizzaSize] = useState("l");
+  const [pizzaSize, setPizzaSize] = useState("l");
 
-    const [ingredients, setIngredients] = useState({
-        meat: false,
-        cheese: false,
-        vegetarian: false,
-    });
+  const [ingredients, setIngredients] = useState({
+    meat: false,
+    cheese: false,
+    vegetarian: false,
+  });
 
-    const userIngredients = {
-        meat: "Double Meat",
-        cheese: "Double Cheese",
-        vegetarian: "Vegetarian",
-    };
+  const userIngredients = {
+    meat: "Double Meat",
+    cheese: "Double Cheese",
+    vegetarian: "Vegetarian",
+  };
 
-    const handlePizzaName = (e) => {
-        setPizzaName(e.target.value);
-    };
+  const handlePizzaName = (e) => {
+    setPizzaName(e.target.value);
+  };
 
-    const handlePizzaSize = (e) => {
-        setPizzaSize(e.target.value);
-    };
+  const handlePizzaSize = (e) => {
+    setPizzaSize(e.target.value);
+  };
 
-    function handleIngredients(e) {
-        setIngredients({ ...ingredients, [e.target.name]: e.target.checked });
-    }
+  function handleIngredients(e) {
+    setIngredients({ ...ingredients, [e.target.name]: e.target.checked });
+  }
 
-    function handleOrderPizza() {
-        alert(
-            "You ordered pizza: " +
-                pizzaName +
-                " Size:" +
-                pizzaSize.toUpperCase() +
-                " Ingredients: " +
-                getIngredients(ingredients)
-        );
-    }
-
-    function getIngredients(ingredientList) {
-        const keys = Object.keys(ingredientList);
-        const resultArr = [];
-        for (const ingredient of keys) {
-            if (ingredientList[ingredient])
-                resultArr.push(userIngredients[ingredient]);
-        }
-        return resultArr.join(", ");
-    }
-
-    return (
-        <>
-            <div>
-                <label htmlFor={pizzaNameId}>
-                    Name of pizza
-                    <select
-                        id={pizzaNameId}
-                        value={pizzaName}
-                        onChange={handlePizzaName}
-                    >
-                        <option value="margarita">margarita</option>
-                        <option value="pepperoni">pepperoni</option>
-                        <option value="4_cheese">4 cheese</option>
-                    </select>
-                </label>
-            </div>
-            <div>
-                <label>
-                    <input
-                        type="radio"
-                        name="pizzaSize"
-                        value="s"
-                        onChange={handlePizzaSize}
-                        checked={pizzaSize === "s"}
-                    ></input>
-                    S
-                </label>
-                <label>
-                    <input
-                        type="radio"
-                        name="pizzaSize"
-                        value="m"
-                        onChange={handlePizzaSize}
-                        checked={pizzaSize === "m"}
-                    ></input>
-                    M
-                </label>
-                <label>
-                    <input
-                        type="radio"
-                        name="pizzaSize"
-                        value="l"
-                        onChange={handlePizzaSize}
-                        checked={pizzaSize === "l"}
-                    ></input>
-                    L
-                </label>
-            </div>
-            <label>
-                <input
-                    type="checkbox"
-                    name="meat"
-                    checked={ingredients.meat}
-                    onChange={handleIngredients}
-                />
-                {userIngredients.meat}
-            </label>
-            <label>
-                <input
-                    type="checkbox"
-                    name="cheese"
-                    checked={ingredients.cheese}
-                    onChange={handleIngredients}
-                />
-                {userIngredients.cheese}
-            </label>
-            <label>
-                <input
-                    type="checkbox"
-                    name="vegetarian"
-                    checked={ingredients.vegetarian}
-                    onChange={handleIngredients}
-                />
-                {userIngredients.vegetarian}
-            </label>
-
-            <button type="button" onClick={handleOrderPizza}>
-                Order Pizza
-            </button>
-        </>
+  function handleOrderPizza() {
+    alert(
+      "You ordered pizza: " +
+        pizzaName +
+        " Size:" +
+        pizzaSize.toUpperCase() +
+        " Ingredients: " +
+        getIngredients(ingredients)
     );
+  }
+
+  function getIngredients(ingredientList) {
+    const keys = Object.keys(ingredientList);
+    const resultArr = [];
+    for (const ingredient of keys) {
+      if (ingredientList[ingredient])
+        resultArr.push(userIngredients[ingredient]);
+    }
+    return resultArr.join(", ");
+  }
+
+  const initialValue = {
+    pizzaName: "margarita",
+    pizzaSize: "",
+    bonusIngredients: [],
+  };
+
+  function handleSubmit(values, actions) {
+    console.log(values);
+    actions.resetForm();
+  }
+
+  return (
+    <Formik
+      initialValues={initialValue}
+      onSubmit={handleSubmit}
+      validationSchema={scheme}
+    >
+      <Form>
+        <div>
+          <label htmlFor={pizzaNameId}>
+            Name of pizza
+            <Field as="select" name="pizzaName" id={pizzaNameId}>
+              <option value="margarita">margarita</option>
+              <option value="pepperoni">pepperoni</option>
+              <option value="4_cheese">4 cheese</option>
+            </Field>
+            <p>
+              <ErrorMessage name="pizzaName" />
+            </p>
+          </label>
+        </div>
+        <div>
+          <label>
+            <Field type="radio" name="pizzaSize" value="s"></Field>S
+          </label>
+          <label>
+            <Field type="radio" name="pizzaSize" value="m"></Field>M
+          </label>
+          <label>
+            <Field type="radio" name="pizzaSize" value="l"></Field>L
+          </label>
+          <p>
+            <ErrorMessage name="pizzaSize" />
+          </p>
+        </div>
+
+        <label>
+          <Field type="checkbox" name="bonusIngredients" value="meat" />
+          {userIngredients.meat}
+        </label>
+        <label>
+          <Field type="checkbox" name="bonusIngredients" value="cheese" />
+          {userIngredients.cheese}
+        </label>
+        <label>
+          <Field type="checkbox" name="bonusIngredients" value="vegetarian" />
+          {userIngredients.vegetarian}
+        </label>
+
+        <button type="submit">Order Pizza</button>
+      </Form>
+    </Formik>
+  );
 }
 
 export default OrderPizza;
